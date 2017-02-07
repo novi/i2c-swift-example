584dcbcfd3eae82a89215f82413eb677889fdf4d
Yusuke Ito <novi.mad@gmail.com>
2017-02-08 00:47:22 +0900

    add slack post demo

--- Sources/AirPressureSensSlackPost/main.swift
+++ Sources/AirPressureSensSlackPost/main.swift
@@ -1,6 +1,52 @@
 import SlackKit
 import I2CDeviceModule
 
+func formatDate(_ date: Date) -> String {
+    let formatter = DateFormatter()
+    //formatter.timeZone = TimeZone(abbreviation: "JST")
+    //formatter.timeZone = TimeZone.current
+    formatter.dateStyle = .short
+    formatter.timeStyle = .short
+    return formatter.string(from: date)
+}
+
+let slack = WebAPI(token: slackAPIToken)
+
+// get current air pressure and temparature
+let device = try getCurrentI2CDevice()
+
+let sensor = BMP180(device: device)
+
+try sensor.reset()
+
+try sensor.calibrate()
+
+let result = try sensor.mesure(oversampling: .oss3)
+
+print("sensor data", result)
+
+// build message
+
+let temp = Float(result.temperature) / 10
+let fahrenheit = 1.8 * Float(result.temperature) / 10 + 32
+let press = Float(result.pressure) / 100 // in hpa
+
+let message = String(format: "🌡%.0f°F(%.1f°C) 🌀%.2fhPa 🕒(%@)", fahrenheit, temp, press, formatDate(Date()))
+
+print(message)
+
+//exit(0)
+
+// post to slack
+slack.sendMessage(channel: sendChannel, text: message, success: { (a, b) in
+    
+    print("slack sent", String(describing: a), String(describing: b))
+    exit(0)
+    
+}) { (error) in
+    print("slack send error", error)
+    exit(-1)
+}
 
 
-print("test")
+RunLoop.main.run()
